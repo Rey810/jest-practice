7b959848dc6104e4fc90520baaf72b7b87609b30
reyvdb <Rey810@users.noreply.github.com>
2020-05-03 11:20:07 +0700

    final array analysis tests

--- functions/analyze.js
+++ functions/analyze.js
@@ -1,24 +1,36 @@
 function analyze(array) {
   let anArray = array;
-  console.log(anArray);
   if (anArray == undefined) {
     throw new Error("Your array is empty or isn't even an array!");
   }
 
   //average
   let average = getAverage(anArray);
+  let min = getMin(anArray);
+  let max = getMax(anArray);
   return {
     average: average,
+    min: min,
+    max: max,
+    length: anArray.length,
   };
 }
 
 function getAverage(array) {
   let sum = array.reduce((total, num) => {
-    total = total + num;
+    total += num;
+    return total;
   }, 0);
   let average = sum / array.length;
-  console.log("average", array.length);
   return average;
+}
+
+function getMin(array) {
+  return Math.min(...array);
+}
+
+function getMax(array) {
+  return Math.max(...array);
 }
 
 export default analyze;
--- tests/analyze.test.js
+++ tests/analyze.test.js
@@ -11,5 +11,26 @@ test("analyze throws an error if the array is empty", () => {
 });
 
 test("analyze returns the average of the array", () => {
-  expect(analyze([10, 20])).toEqual(15);
+  expect(analyze([10, 20])).toMatchObject({ average: 15 });
+});
+
+test("analyze returns the min number in array", () => {
+  expect(analyze([10, 5, 20])).toMatchObject({ min: 5 });
+});
+
+test("analyze returns the max number in array", () => {
+  expect(analyze([10, 5, 20])).toMatchObject({ max: 20 });
+});
+
+test("analyze returns the length of the array", () => {
+  expect(analyze([10, 5, 20])).toMatchObject({ length: 3 });
+});
+
+test("anaylze returns full analysis of average, min, max, and length", () => {
+  expect(analyze([10, 20])).toEqual({
+    average: 15,
+    min: 10,
+    max: 20,
+    length: 2,
+  });
 });
